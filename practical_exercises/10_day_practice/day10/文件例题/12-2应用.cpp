21c451586e8f757e814fe62afa04c5a9edff843d
Tony-ztj <52416675+Tony-ztj@users.noreply.github.com>
2021-08-20 10:13:13 +0800

    Update 12-2应用.cpp

--- practical_exercises/10_day_practice/day10/文件例题/12-2应用.cpp
+++ practical_exercises/10_day_practice/day10/文件例题/12-2应用.cpp
@@ -11,3 +11,31 @@ int main(int argc, char const *argv[])
     system("pause");
     return 0;
 }
+/*
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    char stu[5][10];
+    int i;
+    for(i=0;i<5;i++)
+        cin.getline(stu[i],10,',');
+    for(i=0;i<5;i++)
+        cout<<stu[i]<<" one jiange"<<endl;
+    return 0;
+}
+*/
+/*
+zhu,
+zhu,zhu
+zhu
+zhu
+zhu one jiange
+
+zhu one jiange
+zhu
+zhu
+z one jiange
+ one jiange
+ one jiange
+ */
